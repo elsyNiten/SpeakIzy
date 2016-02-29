a78c058152d81946a4514b4f7a056d804df01b6c
elsy <ansene.elisee@gmail.com>
2016-02-29 18:29:49 +0700

    Modification du parcours d'enregistrement user/tutor

--- FRONT/app/scripts/controllers/routes.js
+++ FRONT/app/scripts/controllers/routes.js
@@ -12,20 +12,32 @@
 			.when('/reservations', {templateUrl: 'templates/reservations.html'})
 			.when('/connexion', {templateUrl: 'templates/connexion.html'})
 			.when('/dashboard', {templateUrl: 'templates/dashboard.html'})
-			//.when('/on-boarding', {templateUrl: 'templates/on-boarding.html', controller: "etapeCtrl"})
-			.when('/on-boarding', {templateUrl: 'templates/connexion.html', controller: "etapeCtrl"})
+			.when('/registration-tutor', {templateUrl: 'templates/registration-tutor.html'})
+			.when('/on-boarding-tutor', {templateUrl: 'templates/on-boarding-tutor.html'})
+			.when('/registration-user', {templateUrl: 'templates/registration-user.html'})
+			.when('/on-boarding', {templateUrl: 'templates/on-boarding.html', controller: "etapeCtrl"})
 			.when('/pratique', {templateUrl: 'templates/pratique.html'})
 			.otherwise({redirectTo: '/'});
 	}]);
 
 	app.controller('etapeCtrl', function($scope) {			
-		$scope.etapes = function() {
-			var etapes = angular.element(document.querySelectorAll('.etapes'));			
+		$scope.etapes = function($index) {
+
+			var etapes = angular.element(document.querySelectorAll('.etapes'));
+			var bullets = angular.element(document.querySelectorAll('.select-etape'));
 			
 			for(var i = 0; i < etapes.length; i++) {
 				var etape = angular.element(document.getElementById(etapes[i].id));
-				etape.addClass("focusEtapes");
-			}			
+				var bullet = angular.element(document.getElementById(bullets[i].id));
+				etape.removeClass("focusEtapes");
+				bullet.removeClass("focus-etape");
+
+				if(i == $index) {
+					etape.addClass("focusEtapes");
+					bullet.addClass("focus-etape");
+				}
+			}
+
 		}				
 	});
 
